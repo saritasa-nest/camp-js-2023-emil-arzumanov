aaa52b54251725a33036c611f0e3751602757b0e
Emil Arzumanov <emil.arzumanov@saritasa.com>
2023-07-17 11:57:21 +0500

    (fix) changes according to requests JC-532

--- apps/vanilla/src/pages/2/logic/diceGenerator.ts
+++ apps/vanilla/src/pages/2/logic/diceGenerator.ts
@@ -7,21 +7,15 @@ import { Publisher } from './publisher';
  * Dice Generator is also Publisher, his subscribers are instances of Player from './player.ts'.
  */
 export class DiceGenerator extends Publisher<Throw> implements Subscriber<number> {
-	/** The result that rolled out when the dice was thrown. */
-	private diceRollResult = 0;
-
 	/**
 	 * @param currentPlayerIndex Index of player that throws dice in this turn.
 	 */
 	public update(currentPlayerIndex: number): void {
-		/** Saves dice roll result in diceRollResult. */
-		this.diceRollResult = this.throwDice(1, 6);
-
 		/**
 		 * Notifies subscribers which Player is making a move and
 		 * what is his result on the current roll of the dice.
 		 */
-		this.notify({ diceRollResult: this.diceRollResult, currentPlayerIndex });
+		this.notify({ diceRollResult: this.throwDice(1, 6), currentPlayerIndex });
 	}
 
 	/**
