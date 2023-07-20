6068ded004eade153ccbb7a563d804adb372ef1b
Emil Arzumanov <emil.arzumanov@saritasa.com>
2023-07-20 12:50:16 +0500

    (fix) all viktors change requests JC-539

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { ApiKeyInterceptor } from '../core/interceptors/anime.interceptor';
 
 import { SharedModule } from './../shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +15,7 @@ import { AnimeModule } from './features/anime/anime.module';
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, SharedModule, AppRoutingModule, AnimeModule, BrowserAnimationsModule, RouterModule],
-	providers: [],
+	providers: [{ provide: HTTP_INTERCEPTORS, useClass: ApiKeyInterceptor, multi: true }],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
