ea75a04fcfd39930f1acdb2ba0e0950cd221f058
Emil Arzumanov <emil.arzumanov@saritasa.com>
2023-08-18 12:53:07 +0500

    Delete 'validated-form.ts' model JC-566

--- libs/core/models/validated-form.ts
+++
@@ -1,5 +0,0 @@
-import { FormControl, FormGroup } from '@angular/forms';
-
-/** Custom type for Form Group. */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ValidatedFormGroupType<T extends Record<string, any>> = FormGroup<{ [key in keyof T]: FormControl<T[key]> }>;
