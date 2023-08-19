c4e650b890c5568465b1fc88829944620901ba37
Emil Arzumanov <22-emil@mail.ru>
2023-08-19 11:44:40 +0500

    Fix JC-556: 1. Update styles in 'anime-details.component' 2. Update 'datesValidator' in 'dates-validate.utils'

--- apps/angular/src/core/utils/dates-validate.utils.ts
+++ apps/angular/src/core/utils/dates-validate.utils.ts
@@ -3,7 +3,7 @@ import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 /** Checks if end date is after start date. */
 export function datesValidator(): ValidatorFn {
 	return (control: AbstractControl): ValidationErrors | null => {
-		if (control.value > control.parent?.get('airedStart')?.value) {
+		if (control.value >= control.parent?.get('airedStart')?.value) {
 			return null;
 		}
 		return { matching: true };
